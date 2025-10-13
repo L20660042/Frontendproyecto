a99f96e557e257ee2e5659fb9caa8989823ce19c
L20660042 <l20660042@matehuala.tecnm.mx>
2025-10-12 20:43:56 -0600

    adaptacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import { Link } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Home";  // Usamos la extensión .tsx para el componente
 import Login from "./pages/Login"; // Usamos .tsx para Login
@@ -7,7 +5,7 @@ import Register from "./pages/Register"; // Usamos .tsx para Register
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/Frontendproyecto"> {/* Agrega el basename para GitHub Pages */}
       {/* Definición de las rutas */}
       <Routes>
         <Route path="/" element={<Homepage />} />
