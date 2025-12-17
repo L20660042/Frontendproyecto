b0335a7a423cfffa4c8c0fae425297169f9badd9
L20660042 <l20660042@matehuala.tecnm.mx>
2025-12-17 17:06:18 -0600

    cerrado por reestructuracion

--- src/components/docente/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as MateriasTable } from './MateriasTable';
-export { default as EstudiantesTable } from './EstudiantesTable';
-export { default as AlertasTable } from './AlertasTable';
-export { default as ReportesTable } from './ReportesTable';
-export { default as EvaluacionDocente } from './EvaluacionDocente';
